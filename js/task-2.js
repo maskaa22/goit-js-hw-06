63e79a1d2588f8d7d620272cabe82ca590f12e2b
maskaa22 <kyrosika565@gmail.com>
2024-09-28 10:06:33 +0300

    check 3 hw

--- js/task-2.js
+++ js/task-2.js
@@ -1,22 +1,22 @@
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
+function makeArray(firstArray, secondArray, maxLength) {
+  const arr = firstArray.concat(secondArray);
+  if (arr.length >= maxLength) {
+    return arr.slice(0, maxLength);
   } else {
-    let newRow = message.slice(0, maxLength);
-    return `${newRow}...`;
+    return arr;
   }
 }
 
-formatMessage('Curabitur ligula sapien', 16);
-formatMessage('Curabitur ligula sapien', 23);
-formatMessage('Vestibulum facilisis purus nec', 20);
-formatMessage('Vestibulum facilisis purus nec', 30);
-formatMessage('Nunc sed turpis a felis in nunc fringilla', 15);
-formatMessage('Nunc sed turpis a felis in nunc fringilla', 41);
+makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3);
+makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4);
+makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3);
+makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2);
+makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4);
+makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0);
 
-console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
-console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
-console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
-console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
